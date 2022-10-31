c6a99a854841f8d66236beb3e9e4ad336bf88b23
muturich <calvinrich20@gmail.com>
2022-10-31 13:24:03 -0500

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+var userInput = window.prompt("How long do you want your password to be")
+
+var passwordLength = parseInt(userInput)
+
+if (isNaN(passwordLength)){
+  window.alert("that's not a number")
+  return
+}
+
+if (passwordLength <8 || passwordLength >128){
+  window.alert("Password lenght must be between 8 and 128 characters")
+  return
+}
+
+var userWantsNumbers = window.confirm("Would you like to include numbers in your password")
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +33,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
