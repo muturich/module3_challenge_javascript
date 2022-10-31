766c007c6bf6f9c9867f1b3f80a8d622e8b16db4
muturich <calvinrich20@gmail.com>
2022-10-31 16:38:24 -0500

    final commit

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharArr = ['!', '@', '#','$','%','^','&','*'];
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseArr = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberArr = ['1' , '2', '3' ,'4', '5', '6' , '7' , '8' , '9','0',];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-
-var userInput = window.prompt("How long do you want your password to be")
-
-var passwordLength = parseInt(userInput)
-
-if (isNaN(passwordLength)){
-  window.alert("that's not a number")
-  return
-}
-
-if (passwordLength <8 || passwordLength >128){
-  window.alert("Password lenght must be between 8 and 128 characters")
-  return
-}
-
-var userWantsNumbers = window.confirm("Would you like to include numbers in your password")
-
-
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  if (correctPrompts) {
+  var newPassword = generatePassword();
+  passwordText.value = newPassword;
+ } else {
+  passwordText.value= "";
+ }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  //*generate password based on the prompts
+    var password = " ";
+    for (var i = 0; i < characterLength; i++) {
+      var randomIndex = Math.floor(Math.random() *choiceArr.length);
+      password = password + choiceArr[randomIndex];
+        }
+          return password;
+  }
+  
+  function getPrompts(){
+    choiceArr=[];
+
+    characterLength = parseInt(prompt("How many characters do you want your password to be?v(Between 8 - 128 characters"));
+
+    if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+      alert ("Character lenght has to be a number,8 - 128 digits. Please try again ");
+      return false;
+    }
+    if (confirm ("Would you like lowercase letter in your password?")){
+      choiceArr = choiceArr.concat(lowerCaseArr);
+    }
+    if (confirm ("Would you like uppercase letter in your password?")){
+      choiceArr = choiceArr.concat(upperCaseArr);
+    }
+    if (confirm ("Would you like special character in your password?")){
+      choiceArr = choiceArr.concat(specialCharArr);
+    }
+    if (confirm ("Would you like numbers in your password?")){
+      choiceArr = choiceArr.concat(numberArr);
+    }
+    return true;
+  }
 
